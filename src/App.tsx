a68767de779053c37a3580a4c32c8d6e2406186d
Alef oliveira <alefolivfeira@gmail.com>
2022-09-10 22:49:12 -0300

    feat: header box content

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import { Header } from './components/Header';
 import './global.css';
+import { PlusCircle } from 'phosphor-react';
+
+import styles from './App.module.css';
+import { TodoBox } from './components/TodoBox';
 
 
 function App() {
@@ -7,11 +11,26 @@ function App() {
   return (
     <>
       <Header />
-      <main>
-        
-      </main>
+      <div className={styles.wrapper}>
+
+        <form className={styles.formEdit} >
+          <input
+            type="text"
+            placeholder="Adicione uma nova tarefa"
+            name=""
+            id=""
+          />
+          <button type="submit">
+            <strong>Criar</strong>
+            <PlusCircle size={32} />
+          </button>
+        </form>
+        <main>
+          <TodoBox />
+        </main>
+      </div>
     </>
-    )
+  )
 }
 
 export default App
---
+++ src/components/TodoBox.tsx
@@ -0,0 +1,49 @@
+import styles from './TodoBox.module.css';
+import Clipboard2 from '../assets/Clipboard.svg';
+import { useState } from 'react';
+
+export const TodoBox = () => {
+
+    const [todo, setTodo] = useState(0);
+
+
+    return (
+        <>
+            <header>
+                <div className={styles.wrapperBoxTodo}>
+                    <div className={styles.countCriated}>
+                        <strong>Tarefas criadas</strong>
+                        <div className={styles.boxNumber}>
+                            <strong>1</strong>
+                        </div>
+                    </div>
+                    <div className={styles.countComplited}>
+                        <strong>Concluídas</strong>
+                        <div className={styles.boxNumber}>
+                            <strong>1</strong>
+                        </div>
+                    </div>
+                </div>
+
+            </header>
+
+            <main>
+                {
+                    !todo ? 
+                        (
+                            <div className={styles.todoContentContainersEmpty}>
+                                <div className={styles.divider} ></div>
+                            <img src={Clipboard2} width={56} alt="" />
+                            <strong>Você ainda não tem tarefas cadastradas</strong>
+                            <p>Crie tarefas e organize seus itens a fazer</p>
+                        </div>
+                        )
+                    :
+                        (
+                            <div>TEm todo</div>
+                        )
+                }
+            </main>
+        </>
+    )
+}
